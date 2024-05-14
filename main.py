b0d46d7b3d43dc7ef2a021c52ca3cfb4f2d0354b
paaavooo <paawo.koivistoinen@gmail.com>
2024-05-14 14:15:40 +0300

    aa

--- main.py
+++ main.py
@@ -3,3 +3,23 @@
 - Sanan valinta
 - 
 """
+sanathelppo = ["kissa", "koira", "talo", "auto", "juna", "kukka", "puisto", "saari", "lumi", "kala"]
+
+sanatkeski = ["kirjasto", "elefantti", "viikset", "pannukakku", "mainoskyltti", "kultaraha", "televisio", "vesiputous", "maailmankaikkeus", "tietokone"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
